8ddc1da6c38c6000e78e21a6689cdb1cb4f1ff27
Nicholas Gallina <nicholasgallina1922@gmail.com>
2026-03-08 16:59:27 -0400

    chore: odds & ends

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,12 +5,12 @@ export default function Footer() {
       <hr className="my-20 border-3"></hr>
       <div className="flex justify-center gap-20 pb-20">
         <a href="/privacypolicy">
-          <h2 className="cursor-pointer hover:scale-110 transition-all duration-200">
+          <h2 className="cursor-pointer hover:scale-110  hover:font-bold transition-all duration-200">
             Privacy Policy
           </h2>
         </a>
         <a href="/cookiepolicy">
-          <h2 className="cursor-pointer hover:scale-110 transition-all duration-200">
+          <h2 className="cursor-pointer hover:scale-110 hover:font-bold transition-all duration-200">
             Cookie Policy
           </h2>
         </a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
       colors: {
         brand: "#006917",
         testcolor: "#ff0000",
+        highlight:"#2d55aa",
       },
     },
   },
-  plugins: [],
+  plugins: []
 };
